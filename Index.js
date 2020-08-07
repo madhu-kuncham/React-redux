9fec7445d22043d0972a3dde1dd9042c8f8db28e
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-08-07 19:41:57 +0530

    first commit

--- Index.js
+++ Index.js
@@ -1,6 +1,25 @@
 const BUY_CAKE = 'BUY_CAKE'
 
-{
-    type: BUY_CAKE,
-    info: 
+function buyCake () {
+    return {
+        type: BUY_CAKE,
+        info: 'first redux action'
+    }
+}
+
+// (previousState , action) => newState
+
+const initialState= {
+    numOfCakes : 10
+}
+
+const reducer = (state= initialState , action) => {
+    switch (action.type) {
+        case BUY_CAKE: return{
+            ...state,
+            numOfCakes: state.numOfCakes-1
+        }
+
+        default: return state
+    }
 }
